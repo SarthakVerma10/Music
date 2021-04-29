7b65d3eb9a101477b5410b09dc55f3ef26d5b87f
Sarthak <sarthakverma0097@gmail.com>
2021-04-29 17:24:43 +0530

    bug fixes

--- actions/addSong.php
+++ actions/addSong.php
@@ -1,6 +1,6 @@
 <?php
     $name = $_POST["song"];
     $category = explode("/",$_POST["category"]);
-    $mysqli = new mysqli("localhost", "root", "1234", "music", 3306);
+    $mysqli = new mysqli("db4free.net", "cistbuzz", "Kake_verma10", "playlist_at_mood", 3306);
     $mysqli->query("INSERT INTO SONGS(SNAME, CATEGORY) VALUES ('$name','$category[1]')");
     echo $name;
--- actions/removeCat.php
+++ actions/removeCat.php
@@ -1,7 +1,7 @@
 <?php
     $cat  = $_POST['catName'];
     echo $cat;
-    $mysqli = new mysqli("localhost", "root", "1234", "music", 3306);
+    $mysqli = new mysqli("db4free.net", "cistbuzz", "Kake_verma10", "playlist_at_mood", 3306);
     $mysqli->query("DELETE FROM SONGS WHERE CATEGORY='$cat'");
     $mysqli->query("DELETE FROM CATEGORY WHERE name='$cat'");
     
